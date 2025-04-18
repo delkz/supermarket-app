345b0b5d54c6c6abd678870ea89f176ad435a0c2
David Silva <davidlsilva.sp@gmail.com>
2025-04-18 11:19:20 -0300

    feat: adiciona componente Header com links de navegação

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <header className="bg-base-200 p-4">
+            <nav className="navbar">
+            <ul className="menu menu-horizontal px-1">
+                <li><Link href="/" className="btn btn-ghost">📃 Listagem</Link></li>
+                <li><Link href="/product/register" className="btn btn-ghost">📦 Novo produto</Link></li>
+                <li><Link href="/brand/register" className="btn btn-ghost">🗂️ Nova marca</Link></li>
+            </ul>
+            </nav>
+        </header>
+    )
+}
