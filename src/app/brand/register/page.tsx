ce7ae9bfa595b1b54695b1a5432cf8fae85e17a6
David Silva <davidlsilva.sp@gmail.com>
2025-04-19 19:28:53 -0300

    feat: melhorias visuais

--- src/app/brand/register/page.tsx
+++ src/app/brand/register/page.tsx
@@ -6,7 +6,7 @@ export default function RegisterPage() {
 
     return <main className="p-6 w-full container mx-auto">
         <h1 className="text-3xl font-bold mb-6">🗂️ Cadastrar nova marca</h1>
-        <div className="form-control mb-6 w-full max-w-sm">
+        <div className="form-control mb-6 w-full">
             <BrandForm/>
         </div>
     </main>
