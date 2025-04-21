b9fbc8d6b831ea523602b7f3422a7bd2254f527a
David Silva <davidlsilva.sp@gmail.com>
2025-04-21 14:44:57 -0300

    feat: adiciona o footer e ajusta o header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="bg-base-200 ">
             <nav className="navbar">
-            <ul className="menu menu-horizontal px-1 p-6 w-full container mx-auto">
+            <ul className="menu menu-horizontal flex-col md:flex-row px-1 p-6 w-full container mx-auto">
                 <li><Link href="/" className="btn btn-ghost">📃 Listagem</Link></li>
                 <li><Link href="/product/register" className="btn btn-ghost">📦 Novo produto</Link></li>
                 <li><Link href="/brand/register" className="btn btn-ghost">🗂️ Nova marca</Link></li>
